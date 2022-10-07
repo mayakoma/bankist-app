21962293e454850166567d8f40730fe77a5985b9
maya koma <mayakoma@gmail.com>
2022-10-07 15:03:50 +0300

    scroll

--- bankist-app/script.js
+++ bankist-app/script.js
@@ -35,9 +35,5 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
