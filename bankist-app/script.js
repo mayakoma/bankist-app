4ee3a885187bcbe3c3b145e2558b8e78f1573851
maya koma <mayakoma@gmail.com>
2022-10-09 13:25:33 +0300

    section

--- bankist-app/script.js
+++ bankist-app/script.js
@@ -115,8 +115,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
-// const initialCoords = section1.getBoundingClientRect();
-// window.addEventListener('scroll', function () {
-//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// });
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
